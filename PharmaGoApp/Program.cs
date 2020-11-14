6df4cecebaccbdb0edf35d842905d80249e69395
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2020-11-14 22:05:23 +0700

    Connection with Google Cloud, aka Firestore By Andrey

--- PharmaGoApp/Program.cs
+++ PharmaGoApp/Program.cs
@@ -8,13 +8,28 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Google.Cloud.Firestore;
+using Google.Cloud.Firestore.V1;
+
 
 namespace PharmaGoApp
 {
     public class Program
     {
+        //Path to json file with credentials
+        static string keyFilepath = "path/to/file";
+        //Actual project id from Google cloud
+        static string projectId = "pharmaproject-777";
+
         public static void Main(string[] args)
         {
+
+            //Code for GOOGLE AUTHENTICATION CREDENTIALS part 
+
+            var jsonString = File.ReadAllText(keyFilepath);
+            var builder = new FirestoreClientBuilder { JsonCredentials = jsonString };
+            FirestoreDb db = FirestoreDb.Create(projectId, builder.Build());
+
+            //standard project setup
             CreateWebHostBuilder(args).Build().Run();
         }
 
