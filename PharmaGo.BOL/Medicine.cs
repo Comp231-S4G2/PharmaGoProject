cb198230fc21764364b2c284bf9c2833952b674e
Harsimran Kaur <74160149+HarsimranKaur1997@users.noreply.github.com>
2020-11-14 21:28:30 +0530

    Business object layer creation
    POCO class for Medicines is attached

---
+++ PharmaGo.BOL/Medicine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmaGo.BOL
+{
+    public class Medicine
+    {
+        public long MedId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
