5d739482b74fbcdd912cf8a94f044ec08c439c27
hossai80 <nahidhossain23@hotmail.com>
2020-11-16 02:28:10 +0530

    Update PGADbContext.cs
    DbSet for each of the POCO class is attached

--- PharmaGo.DAL/PGADbContext.cs
+++ PharmaGo.DAL/PGADbContext.cs
@@ -1,5 +1,6 @@
 ﻿#region Namespaces
 using Microsoft.EntityFrameworkCore;
+using PharmaGo.BOL;
 #endregion
 
 
@@ -12,5 +13,11 @@ namespace PharmaGo.DAL
         {
             optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=GPADb;Trusted_Connection=True;");
         }
+
+        public DbSet<GPAUser> GPAUsers { get; set; }
+        public DbSet<Medicine> Medicines { get; set; }
+        public DbSet<StockMedicine> StockMedicines { get; set; }
+        public DbSet<Pharmacy> Pharmacies { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
     }
 }
