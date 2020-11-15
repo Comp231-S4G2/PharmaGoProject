11ea5468bbf8cd19ea4391d3daac925a36248f2a
Yogesh-34 <73706141+Yogesh-34@users.noreply.github.com>
2020-11-16 00:39:08 +0530

    Update GPAUser.cs
    POCO class for Application users is created

--- PharmaGo.BOL/GPAUser.cs
+++ PharmaGo.BOL/GPAUser.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿/// <summary>
+/// Application User Class 
+/// </summary>
 
 namespace PharmaGo.BOL
 {
     public class GPAUser
     {
-        public int MyProperty { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public long? PharmaId { get; set; }
     }
 }
