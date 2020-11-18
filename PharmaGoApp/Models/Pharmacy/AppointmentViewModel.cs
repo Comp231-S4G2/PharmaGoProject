9178ed32d475a76e4f14caf72193fa845f12498c
gurvansh1 <gsin1532@my.centennialcollege.ca>
2020-11-19 01:08:41 +0530

    Pharmacist Home Screen Attached
    Mocked it with Controller

---
+++ PharmaGoApp/Models/Pharmacy/AppointmentViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaGoApp.Models.Pharmacy
+{
+    public class AppointmentViewModel
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string CustomerName { get; set; }
+        public string Age { get; set; }
+
+        [Required]
+        public string TimeSlot { get; set; }
+
+        [Required]
+        public DateTime DOB { get; set; }
+    }
+}
