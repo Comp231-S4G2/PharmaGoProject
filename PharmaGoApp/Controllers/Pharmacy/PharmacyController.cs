a06176dd13d07515430d331ec4fcd56fac9a743f
HarsimranKaur1997 <74160149+HarsimranKaur1997@users.noreply.github.com>
2020-11-27 02:00:22 +0530

    Pharmacist can browse all requested Prescription
    Pharmacist can browse all requested Prescription

--- PharmaGoApp/Controllers/Pharmacy/PharmacyController.cs
+++ PharmaGoApp/Controllers/Pharmacy/PharmacyController.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PharmaGo.BLL;
 using PharmaGoApp.Models.Pharmacy;
 
 namespace PharmaGoApp.Controllers.Pharmacy
 {
     public class PharmacyController : Controller
     {
+        ICustomerPrescriptionBS customerPrescriptionBS;
+        IGPAUsersBS gPAUsersBS;
         public static List<AppointmentViewModel> appointments = new List<AppointmentViewModel>()
         {
             new AppointmentViewModel(){Id=1,CustomerName="Neeraj",TimeSlot="9.00 AM",Age="20"},
             new AppointmentViewModel(){Id=2,CustomerName="Fred",TimeSlot="9.30 AM",Age="25"},
             new AppointmentViewModel(){Id=3,CustomerName="Neeraj",TimeSlot="10.00 AM",Age="24"},
         };
-        public PharmacyController()
+        public PharmacyController(ICustomerPrescriptionBS _customerPrescriptionBS, IGPAUsersBS _gPAUsersBS)
         {
-
+            customerPrescriptionBS = _customerPrescriptionBS;
+            gPAUsersBS = _gPAUsersBS;
         }
         public IActionResult Index()
         {
@@ -53,5 +57,18 @@ namespace PharmaGoApp.Controllers.Pharmacy
             return RedirectToAction("Index");
         }
 
+        public IActionResult GetPrescriptions()
+        {
+            var result = customerPrescriptionBS.GetCustomerPrescriptions().Select(x=>
+            new PrescriptionValidateViewModel()
+            { 
+                FilePath=x.PrescriptionPath,//.Replace("/", "\\"),
+                CustomerId =x.UserId,
+                CustomerName=x.user.UserName,
+                PrescriptionId=x.Id
+            
+            });
+            return View(result);
+        }
     }
 }
---
+++ PharmaGoApp/Models/Pharmacy/PrescriptionValidateViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmaGoApp.Models.Pharmacy
+{
+    public class PrescriptionValidateViewModel
+    {
+        public string FilePath { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerId { get; set; }
+        public long PrescriptionId { get; set; }
+    }
+}
