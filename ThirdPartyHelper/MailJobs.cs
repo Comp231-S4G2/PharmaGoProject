825afc462187e4366f828d49a352b2430b99a3fd
sali268 <73714553+sali268@users.noreply.github.com>
2020-12-04 22:15:25 +0530

    email warning
    email warning of account deletion

---
+++ ThirdPartyHelper/MailJobs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using ThirdPartyHelper.Model;
+
+namespace ThirdPartyHelper
+{
+    public class MailJobs
+    {
+        public static void SendEmail(SendEmailModel sendMail)
+        {
+            try
+            {
+                if (IsSendEmailModelValid(sendMail))
+                {
+                    MailMessage message = new MailMessage();
+                    SmtpClient smtp = new SmtpClient();
+                    message.From = new MailAddress(sendMail.MailFrom);
+
+                    foreach (var receipientMail in sendMail.ReceiverEmails)
+                        message.To.Add(new MailAddress(receipientMail));
+
+                    message.Subject = sendMail.Subject;
+                    message.IsBodyHtml = true;
+                    message.Body = sendMail.HtmlBody;
+                    smtp.Port = sendMail.PortNumber;
+                    smtp.Host = sendMail.HostIP; //specify  host  or DN:Domain Name
+                    smtp.EnableSsl = true;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential(sendMail.MailFrom, sendMail.Credential);
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.Send(message);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static bool IsSendEmailModelValid(SendEmailModel sendMail)
+        {
+            try
+            {
+                if (sendMail.Credential.Trim() != "" && sendMail.HostIP.Trim() != ""
+                    && sendMail.HtmlBody.Trim() != "" && sendMail.MailFrom.Trim() != ""
+                    && sendMail.PortNumber > 0 && sendMail.ReceiverEmails.Count > 0 && sendMail.Subject.Trim() != "")
+                    return true;
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ThirdPartyHelper/Model/SendEmailModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ThirdPartyHelper.Model
+{
+    public class SendEmailModel
+    {
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public List<string> ReceiverEmails { get; set; }
+
+        [Required]
+        public string HtmlBody { get; set; }
+
+        [Required]
+        public string MailFrom { get; set; }
+
+        [Required]
+        public string Credential { get; set; }
+
+        [Required]
+        public string HostIP { get; set; }
+
+        [Required]
+        public int PortNumber { get; set; }
+    }
+}
